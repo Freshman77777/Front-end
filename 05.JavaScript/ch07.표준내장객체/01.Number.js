147cac47fffc43a0744982f42573db15fe933bc4
Freshman77777 <ehdwp6413@gmail.com>
2022-12-01 13:58:00 +0900

    JS

---
+++ 05.JavaScript/ch07.표준내장객체/01.Number.js
@@ -0,0 +1,10 @@
+// 기본 자료형 - primitive type
+let number = 273.1;
+let number2 = Number('273.1');
+
+console.log(number === number2);            // 두 가지 방법이 동일
+
+
+// 객체 자료형 - Wrapper class
+let numObj = Number('273.1');
+
