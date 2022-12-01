6157ee577cc31b415dc9d3530b9995671e61fa48
Freshman77777 <ehdwp6413@gmail.com>
2022-12-01 17:45:22 +0900

    JS

--- 05.JavaScript/ch07.표준내장객체/01.Number.js
+++ 05.JavaScript/ch07.표준내장객체/01.Number.js
@@ -7,4 +7,24 @@ console.log(number === number2);            // 두 가지 방법이 동일
 
 // 객체 자료형 - Wrapper class
 let numObj = Number('273.1');
+console.log(number == numObj);          //값은 같다.
+console.log(number === numObj);         //Number, Object
+
+let str = '과자:1500';
+console.log(str.split(':'));
+
+let strObj = new String('과자:1500');
+console.log(strObj.split(':'));
+
+const obj = Math.PI;
+console.log(pi);
+console.log(pi.toExponential());        //지수 형태
+console.log(pi.toFixed(5));             //소수점 이하 자릿수  
+console.log(pi.toPrecision(3));         //유효숫자 자릿수
+
+console.log(Number.MAX_VALUE, Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER);
+console.log(Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+console.log(Math.log(2, 53)-1);   //9007199254740991, Number.MAX_SAFE_INTEGER와 동일하다.
+
 
